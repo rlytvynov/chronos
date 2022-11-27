6aa270343de5a561f3aad0cf9e7748939c075919
dedvnutrikirilla <93326037+dedvnutrikirilla@users.noreply.github.com>
2022-11-27 13:58:48 +0200

    Update routesAuth.js

--- chronos-backend/routes/routesAuth.js
+++ chronos-backend/routes/routesAuth.js
@@ -4,10 +4,11 @@ module.exports = function (server, opts, done) {
     server.post('/api/auth/login', controler.loginer);
     server.post('/api/auth/register', controler.register);
     server.get('/api/auth/logout', controler.logouter);
-    server.get('/api/auth/me', controler.authMe);
     server.post('/api/auth/passwordReset', controler.reseter);
     server.get('/api/auth/passwordReset/:token', controler.resetConfirmer);
     server.get('/api/auth/confirmEmail/:token', controler.mailConfirmer);
+    
+    server.get('/api/check/me', controler.authMe);
 
     done();
 }
